15dfcbac249224675dfe9ffaae29230dbdc18dc3
tkmoon555 <106902441+tkmoon555@users.noreply.github.com>
2023-02-27 00:54:38 +0900

    Add files via upload

---
+++ scripts/test/demo.py
@@ -0,0 +1,44 @@
+import pybullet as p
+import pybullet_data
+import time
+
+try:
+    p.connect(p.GUI)  # Connect to the PyBullet physics server
+except p.error as e:
+    # if handle the error, connect to xserver 
+    p.connect(p.DIRECT)  # Connect to the PyBullet physics server
+finally:
+    # Code to execute regardless of whether an error occurred
+    print("This will always execute")
+p.setAdditionalSearchPath(pybullet_data.getDataPath())  # optionally
+
+useFixedBase = True
+flags = p.URDF_INITIALIZE_SAT_FEATURES
+
+ground_pos = [0,0,-0.625]
+ground = p.loadURDF("./world/ground.urdf", ground_pos, flags = flags, useFixedBase=useFixedBase)
+robot_pos = [0,0,0.5]
+robot_id = p.loadURDF("./simplebot_v10/model.urdf", robot_pos)
+
+
+
+p.setPhysicsEngineParameter(numSolverIterations=10)
+p.changeDynamics(robot_id, -1, linearDamping=0, angularDamping=0)
+
+
+
+p.setGravity(0, 0,-0.1)  # Set the gravity to -9.81 m/s^2 in the z-direction
+p.setTimeStep(1.0/240.0)   # Set the time step to 1/240 seconds
+p.setRealTimeSimulation(1)
+
+
+
+#for i in range(10000):  # Run the simulation for 1000 steps
+while(1):
+    print("*************************")
+    p.stepSimulation()
+    pos, orn = p.getBasePositionAndOrientation(robot_id)  # Get the position and orientation of the robot's base link
+    print("Robot position:", pos)
+    print("Robot orientation:", orn)
+
+#p.disconnect()
